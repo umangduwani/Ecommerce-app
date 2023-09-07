61a8a38da036b9fa9a432a855deff57c352364e7
umangduwani <143645445+umangduwani@users.noreply.github.com>
2023-09-08 00:12:00 +0530

    Create Home.jsx

---
+++ src/Components/Home Page/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Umang's Sneakers Spectrum</h1>
+      <p>Here You will find a lot of variety of Sneakers all. This is the best you can get.</p>
+      <p>Take a look at our wonderful range of products to choose from</p>
+    </div>
+  )
+}
+
+export default Home
