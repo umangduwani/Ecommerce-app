c4b649399fca34ff78c6b61acc6e32c60dc01e5d
umangduwani <143645445+umangduwani@users.noreply.github.com>
2023-09-08 00:16:03 +0530

    Create Successful.jsx

---
+++ src/Components/Successful/Successful.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Successful = () => {
+  return (
+    <div>
+        <h1 className='text-center text-3xl mt-20'>Congratulations your order is successful !</h1>
+        <h1 className='text-center text-3xl mt-26 capitalize'>jo tha yahi tak tha<span aria-label="a rocket blasting off" role="img">🚀</span></h1>
+        <h1 className='text-center text-3xl mt-20'>Your Order will arrive on 31st September 2023, Thank You !</h1>
+    </div>
+  )
+}
+
+export default Successful
