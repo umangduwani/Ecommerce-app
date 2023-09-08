eace523df9a19f157cdec1e4d13a3dbfcb9ace2d
umangduwani <143645445+umangduwani@users.noreply.github.com>
2023-09-08 07:41:09 +0530

    Update Home.jsx

--- src/Components/Home Page/Home.jsx
+++ src/Components/Home Page/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import Navbar from '../Navbar/Navbar'
 
 const Home = () => {
   return (
-    <div>
-      <h1>Umang's Sneakers Spectrum</h1>
-      <p>Here You will find a lot of variety of Sneakers all. This is the best you can get.</p>
-      <p>Take a look at our wonderful range of products to choose from</p>
+    <div className='bg-blue-200'>
+      <Navbar/>
+      <h1 className='text-center text-6xl'>Umang's Sneakers Spectrum</h1>
+      <p className='text-center text-3xl mt-15'>Here You will find a lot of variety of Sneakers all. This is the best you can get.</p>
+      <p className='text-center text-3xl mt-20'>Take a look at our wonderful range of products to choose from</p>
     </div>
   )
 }
